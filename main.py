2811ee64239c61083814b05fcd8d9a4d0cd385ed
Kamci0 <mgkamil@wp.pl>
2023-04-14 15:16:05 +0200

    wszystko tu zrobiłem

--- main.py
+++ main.py
@@ -8,8 +8,23 @@ def isprime(num):
         for i in range(2, int(math.sqrt(num)) + 1):
             if num % i == 0:
                 return False
-            else:
-                return True
+            return True
+
+
+def sito(nums):
+    numbers = [True for _ in range(1, nums)]
+    primes = []
+    for i in range(1, len(numbers)):
+        if i <= 1:
+            numbers[i] = False
+        else:
+            if numbers[i]:
+                primes.append(i)
+                for j in range(i, len(numbers), i):
+                    numbers[j] = False
+
+    return primes
 
 
 print(isprime(47))
+print(sito(15))
